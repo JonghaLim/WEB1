10d3367d1f923418829f318f6f1c5b5d24d45398
JonghaLim <88878526+JonghaLim@users.noreply.github.com>
2022-05-04 15:01:10 +0900

    자바스크립트 alert, prompt, confirm을 이용한 상호작용
    자바스크립트 alert, prompt, confirm을 이용한 상호작용 공부
    위 함수들은 모두 모달 창을 띄워주는데, 모달 창이 떠 있는 동안은 스크립트의 실행이 일시 중단됩니다. 
    사용자가 창을 닫기 전까지 나머지 페이지와 상호 작용이 불가능합니다.
    세 함수엔 아래와 같이 두 가지 제약사항이 있습니다.
    1. 모달 창의 위치는 브라우저가 결정하는데, 대개 브라우저 중앙에 위치합니다
    2. 모달 창의 모양은 브라우저마다 다릅니다. 개발자는 창의 모양을 수정할 수 없습니다.

---
+++ interaction.js
@@ -0,0 +1,13 @@
+alert("Hello"); // Hello 메세지 창
+
+// prompt(title, [default]) title: 사용자에게 보여줄 문자열, default 입력 필드의 초깃값(선택 값) 
+
+let age = prompt('나이를 입력해주세요', 100);
+
+alert(`당신의 나이는 ${age}살 입니다.`); // 당신의 나이는 100살 입니다.
+
+// confirm(question); 확인버튼를 누르면 true, 그 외의 경우는 false를 반환
+
+let isBoss = confirm("당신이 주인인가요?");
+
+alert(isBoss); //확인 버튼을 눌렀다면 true가 출력
